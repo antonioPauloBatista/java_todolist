f596c5d4e774edb4d15047e1edbed08bba364e33
antonio <apbj1986@gmail.com>
2023-10-12 18:40:34 -0300

    inicial

---
+++ src/main/java/br/com/bastista/todolist/user/UserController.java
@@ -0,0 +1,37 @@
+package br.com.bastista.todolist.user;
+
+import java.net.http.HttpRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity Create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("usuario ja cadastrado");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @GetMapping("/")
+    public String List() {
+
+        return "Ok";
+    }
+}
